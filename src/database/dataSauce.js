f1f0049d345d97d823feab36d77ec15574c539e0
YadaaMac1 <YadaaMac1@gmail.com>
2022-10-27 18:30:18 -0300

    adds planetscale url

--- src/database/dataSauce.js
+++ src/database/dataSauce.js
@@ -3,12 +3,13 @@ import { TodoEntity } from "../models/todos.js";
 import { UserEntity } from "../models/users.js";
 
 export const dataSauce = new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 2345,
-  username: "user",
-  password: "password",
-  database: "db",
+  type: "mysql",
+  //host: "localhost",
+  //port: 2345,
+  //username: "user",
+  //password: "password",
+  //database: "db",
   synchronize: true,
   entities: [TodoEntity, UserEntity],
+  url: "mysql://localhost:3309",
 });
